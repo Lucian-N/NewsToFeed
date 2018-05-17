5b63539ae651d31b05f87670143b4c64a8721fdf
Lucian-N <lucian_niculescu@ymail.com>
2018-05-17 20:43:02 +0300

    Add news class

---
+++ app/src/main/java/com/example/lucian_pc/newstofeed/News.java
@@ -0,0 +1,11 @@
+package com.example.lucian_pc.newstofeed;
+
+public class News {
+
+    private long newsDate;
+    private String newsTitle;
+    private String newsUrl;
+    private int newsImage = NO_IMAGE;
+    private static final int NO_IMAGE = 1;
+
+}
